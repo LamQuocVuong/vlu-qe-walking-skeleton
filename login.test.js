0ef8f13998a30bb37628cd6469603887e69b37f9
LamQuocVuong <banhthia.040676@gmail.com>
2026-09-16 13:44:58 +0700

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -1,13 +1,13 @@
 const login = require("./login");
 
-test("Login successfully with correct username and password", () => {
+test("Login with correct username and password", () => {
     expect(login("admin", "123")).toBe(true);
 });
 
-test("Login failed with incorrect password", () => {
+test("Login with incorrect password", () => {
     expect(login("admin", "1234")).toBe(false);
 });
 
-test("Login failed with incorrect username", () => {
+test("Login with incorrect username", () => {
     expect(login("user", "123")).toBe(false);
 });
